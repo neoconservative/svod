68528ca2c42f38ad48799ddeec13f28e43235686
Sergey <goblinnefs@yandex.ru>
2018-10-31 16:35:35 +0600

    v1.1

--- js/buttonup.js
+++ js/buttonup.js
@@ -1,3 +1,5 @@
+
+//
 var limit     = $(window).height()/3,
     $backToTop = $('#back-to-top');
 
@@ -17,3 +19,21 @@ $backToTop.click(function () {
     }, 1500);
     return false;
 });
+
+
+
+
+// initMap() - функция инициализации карты
+function initMap() {
+    // Координаты центра на карте. Широта: 56.2928515, Долгота: 43.7866641
+    var centerLatLng = new google.maps.LatLng(56.2928515, 43.7866641);
+    // Обязательные опции с которыми будет проинициализированна карта
+    var mapOptions = {
+        center: centerLatLng, // Координаты центра мы берем из переменной centerLatLng
+        zoom: 8               // Зум по умолчанию. Возможные значения от 0 до 21
+    };
+    // Создаем карту внутри элемента #map
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
+// Ждем полной загрузки страницы, после этого запускаем initMap()
+google.maps.event.addDomListener(window, "load", initMap);
